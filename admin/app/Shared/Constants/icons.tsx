ea1f912bfce51640540c2ba31d67d50760310d84
Cedrick C. Alegsao <133325847+cedrickcuencaalegsao@users.noreply.github.com>
2026-03-09 18:47:50 +0800

    update icons

--- admin/app/Shared/Constants/icons.tsx
+++ admin/app/Shared/Constants/icons.tsx
@@ -1,6 +1,23 @@
-import { Eye, EyeOff } from "lucide-react";
+import {
+  Eye,
+  EyeOff,
+  LayoutDashboard,
+  Users,
+  UserCog,
+  FileText,
+  Settings,
+  LogOut,
+  User,
+} from "lucide-react";
 
 export const ICONS = {
   eye_on: Eye,
   eye_off: EyeOff,
+  dashboard:LayoutDashboard,
+  users:Users,
+  user_cog: UserCog,
+  file_text: FileText,
+  settings: Settings,
+  logout: LogOut,
+  user: User,
 };
